d147e2c2419c4824aff3c33213d60c4964e1b85d
Wycki <barrackronald.gmail.com>
2025-09-17 02:22:08 +0300

    Disable Turbopack, and swith to webpack build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import("next").NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -9,6 +8,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    turbo: {
+      enabled: false, // 👈 disable Turbopack, force Webpack build
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
